ab0ca5bde89c154b66da20e15e1dd8cf54792347
Simon Simonsson <simon_bklina@hotmail.com>
2022-10-16 13:00:31 +0200

    Klar!

--- Assignment_2/MainWindow.xaml.cs
+++ Assignment_2/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Assignment_2.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Assignment_2
 {
@@ -20,9 +23,85 @@ namespace Assignment_2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<ContactPerson> contacts;
+
         public MainWindow()
         {
             InitializeComponent();
+            contacts = new ObservableCollection<ContactPerson>();
+            lv_Contacts.ItemsSource = contacts;
+        }
+        // Metod för att lägga till kontakter till listan (knapp)
+        private void btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            var contact = contacts.FirstOrDefault(x => x.Email == tb_Email.Text);
+            if (contact == null)
+            {
+                contacts.Add(new ContactPerson
+                {
+                    FirstName = tb_FirstName.Text,
+                    LastName = tb_LastName.Text,
+                    Email = tb_Email.Text,
+                    StreetName = tb_StreetName.Text,
+                    PostalCode = tb_PostalCode.Text,
+                    City = tb_City.Text
+                });
+            }
+            else
+            {
+                MessageBox.Show("Det finns redan en kontakt med samma e-postadress.");
+            }
+
+            ClearFields();
+        }
+        // Rensar fälten där man skriver information om en kontakt
+        private void ClearFields()
+        {
+            tb_FirstName.Text = "";
+            tb_LastName.Text = "";
+            tb_Email.Text = "";
+            tb_StreetName.Text = "";
+            tb_PostalCode.Text = "";
+            tb_City.Text = "";
+        }
+        // Metod för att kunna ta bort kontaker från listan (knapp)
+        private void btn_Remove_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var contact = (ContactPerson)button!.DataContext;
+
+            contacts.Remove(contact);
+        }
+        // Ser all detaljerad information om en kontakt
+        private void lv_Contacts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var contact = (ContactPerson)lv_Contacts.SelectedItems[0]!;
+            tb_FirstName.Text = contact.FirstName;
+            tb_LastName.Text = contact.LastName;
+            tb_Email.Text = contact.Email;
+            tb_StreetName.Text = contact.StreetName;
+            tb_PostalCode.Text = contact.PostalCode;
+            tb_City.Text = contact.City;
+
+        }
+        // Metod för att kunna uppdatera kontakternas information (knapp)
+        private void btn_Update_Click(object sender, RoutedEventArgs e)
+        {
+            var contact = (ContactPerson)lv_Contacts.SelectedItems[0]!;
+            
+            {
+                contact.FirstName = tb_FirstName.Text;
+                contact.LastName = tb_LastName.Text;
+                contact.Email = tb_Email.Text;
+                contact.StreetName = tb_StreetName.Text;
+                contact.PostalCode = tb_PostalCode.Text;
+                contact.City = tb_City.Text;
+
+                lv_Contacts.Items.Refresh();
+            }
+
+            ClearFields();
         }
     }
 }
+
---
+++ Assignment_2/Models/ContactPerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2.Models
+{
+    public class ContactPerson
+    {   
+        // Lagt till properties (all information som har med kontaken att göra)
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string StreetName { get; set; } = null!;
+        public string PostalCode { get; set; } = null!;
+        public string City { get; set; } = null!;
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
